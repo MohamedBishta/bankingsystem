495f06a9a1343c627da60b46960584587d5140e8
Mohamed Bishta <muhamadbishta@gmail.com>
2024-05-09 17:06:30 +0300

    finishing banking  system

--- bin/untitled3.dart
+++ bin/untitled3.dart
@@ -1,14 +1,8 @@
 
-import 'package:untitled3/Rectangle.dart';
-import 'package:untitled3/Triangle.dart';
+import 'package:untitled3/BankingSystem/BankingSystem.dart';
 
 void main(List<String> arguments) {
-  Rectangle rectangle = Rectangle(20, 10);
-  print('Rectangle Area is ${rectangle.calculateArea()}');
-  rectangle.draw();
 
-  print("--------------------------------");
-  Triangle triangle = Triangle(20, 25);
-  print('Triangle Area is  ${triangle.calculateArea()}');
-  triangle.draw();
+  BankingSystem system = BankingSystem();
+  system.showMenu();
 }
--- lib/BankingSystem/main.dart
+++
@@ -1,7 +0,0 @@
-
-import 'package:untitled3/BankingSystem/BankingSystem.dart';
-
-void main(){
-  BankingSystem system = BankingSystem();
-  system.showMenu();
-}
